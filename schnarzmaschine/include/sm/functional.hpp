c0834a01703d7c585ea81b3e2f7d02833f0a85f6
voidstorm <flo.putz@gmail.com>
2018-10-17 17:25:01 +0200

    updates

--- schnarzmaschine/include/sm/functional.hpp
+++ schnarzmaschine/include/sm/functional.hpp
@@ -30,75 +30,75 @@ constexpr auto enum_cast(E e) noexcept
 }
    
 template<typename Function, typename... Arguments>
-INLINE auto partial(Function &&func, Arguments&&... args) {
+INLINE constexpr auto partial(Function &&func, Arguments&&... args) {
    return [=](auto&&... rest) {
       return func(std::forward<Arguments>(args)..., std::forward<Arguments>(rest)...);
    };
 }
 
 template <typename Collection, typename unop>
-INLINE void for_each(Collection &&col, unop &&op) {
+INLINE constexpr void for_each(Collection &&col, unop &&op) {
    std::for_each(std::begin(col), std::end(col), std::forward<unop>(op));
 }
 
 template <typename Collection, typename unop>
-INLINE void generate(Collection &&col, unop &&op) {
+INLINE constexpr void generate(Collection &&col, unop &&op) {
    std::generate(std::begin(col), std::end(col), std::forward<unop>(op));
 }
 
 template <typename Condition, typename Function, typename... Arguments>
-INLINE void while_true(Condition &&cond, Function &&f, Arguments&&... args) {
+INLINE constexpr void while_true(Condition &&cond, Function &&f, Arguments&&... args) {
    while (cond() == true)
       f(std::forward<Arguments>(args)...);
 }
 
 template <typename Condition, typename Function, typename... Arguments>
-INLINE void while_false(Condition &&cond, Function &&f, Arguments&&... args) {
+INLINE constexpr void while_false(Condition &&cond, Function &&f, Arguments&&... args) {
    while (cond() == false)
       f(std::forward<Arguments>(args)...);
 }
 
 template <typename Counter, typename Counter_Function, typename Function, typename... Arguments>
-INLINE void repeat(const Counter &&times, Counter_Function &&cf, Function &&f, Arguments&&... args) {
+INLINE constexpr void repeat(const Counter &&times, Counter_Function &&cf, Function &&f, Arguments&&... args) {
    for (Counter i = 0; i< times; ++i)
       f(cf(std::forward<Counter>(i)), std::forward<Arguments>(args)...);
 }
 
 template <typename Counter, typename Function, typename... Arguments>
-INLINE void repeat(const Counter times, Function &&f, Arguments&&... args) {
+INLINE constexpr void repeat(const Counter times, Function &&f, Arguments&&... args) {
    for (Counter i = 0; i< times; ++i)
       f(std::forward<Counter>(i), std::forward<Arguments>(args)...);
 }
 
 
 template <typename Collection, typename unop>
-INLINE Collection map(Collection col, unop &&op) {
+INLINE constexpr Collection map(Collection col, unop &&op) {
    std::transform(std::begin(col), std::end(col), std::begin(col), std::forward<unop>(op));
    return col;
 }
 
 template <typename Collection, typename binop>
-INLINE Collection zip(Collection &&fc, Collection sc, binop &&op) {
+INLINE constexpr Collection zip(Collection &&fc, Collection sc, binop &&op) {
    std::transform(std::begin(fc), std::end(fc), std::begin(sc), std::begin(sc), std::forward<binop>(op));
    return sc;
 }
 
 template <typename Collection, typename Condition>
-INLINE bool exists(Collection &&col, Condition &&con) {
+INLINE constexpr bool exists(Collection &&col, Condition &&con) {
    auto exist = std::find_if(std::begin(col), std::end(col), std::forward<Condition>(con));
    return exist != col.end();
 }
 
 
 template <typename Collection, typename Predicate>
-INLINE Collection filterNot(Collection col, Predicate &&predicate) {
+INLINE constexpr Collection filterNot(Collection col, Predicate &&predicate) {
    auto returnIterator = std::remove_if(std::begin(col), std::end(col), std::forward<Predicate>(predicate));
    col.erase(returnIterator, std::end(col));
    return col;
 }
 
 template <typename Collection, typename Predicate>
-INLINE Collection filter(Collection col, Predicate &&predicate) {
+INLINE constexpr Collection filter(Collection col, Predicate &&predicate) {
    auto fnCol = filterNot(col, [predicate](typename Collection::value_type i) { return !predicate(i); });
    return fnCol;
 }
