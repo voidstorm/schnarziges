ffeb09fc36c255cd5cdf48a1c42dc479f4e96913
voidstorm <flo.putz@gmail.com>
2019-01-08 21:52:49 +0100

    added alignment

--- schnarzmaschine/include/sm/array.hpp
+++ schnarzmaschine/include/sm/array.hpp
@@ -32,7 +32,7 @@
 
 
 namespace sm {
-template<class T, class Enable = void>
+template<class T, size_t ALIGNMENT= 16, class Enable = void>
 class array final {
 
    //prevent heap alloc
@@ -45,68 +45,75 @@ public:
    INLINE array(const size_t size)
       :
       m_size(size) {
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);	//alloca will create chunk on the stack
-      m_data = new(m_data) T();
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+      m_aligned_data = new(m_data) T();
    }
 
    INLINE array(std::initializer_list<T> l)
       :
       m_size(l.size()) {
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
-      m_data = new(m_data) T();
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+      m_aligned_data = new(m_data) T();
       std::copy(l.begin(), l.end(), this->begin());
    }
 
    INLINE array(const array<typename T> &other)
       :
       m_size(other.m_size) {
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
-      m_data = new(m_data) T();
-      memcpy(m_data, other.m_data, sizeof(T) * m_size);
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+      m_aligned_data = new(m_data) T();
+      memcpy(m_aligned_data, other.m_aligned_data, sizeof(T) * m_size);
    }
 
    INLINE array& operator=(const array<typename T> &other) {
       m_size = other.m_size;
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
-      m_data = new(m_data) T();
-      memcpy(m_data, other.m_data, sizeof(T) * m_size);
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+      m_aligned_data = new(m_data) T();
+      memcpy(m_aligned_data, other.m_aligned_data, sizeof(T) * m_size);
    }
 
    INLINE array& operator=(std::initializer_list<T> other) {
       m_size = other.m_size;
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
-      m_data = new(m_data) T();
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+      m_aligned_data = new(m_data) T();
       std::copy(other.begin(), other.end(), begin());
    }
 
    INLINE array(const array<typename T> &&other) {
       m_size = other.m_size;
       m_data = other.m_data;
+      m_aligned_data = other.m_aligned_data;
       other.m_size = 0;
+      other.m_aligned_data = nullptr;
       other.m_data = nullptr;
    }
 
    INLINE ~array() {
-      m_data->~T();
+      m_aligned_data->~T();
       _freea(m_data);
    }
 
    INLINE T* const data() const {
-      return m_data;
+      return m_aligned_data;
    }
 
    INLINE T* data() {
-      return m_data;
+      return m_aligned_data;
    }
 
    INLINE T& operator[](const size_t idx) const {
       assert(idx < m_size);
-      return m_data[idx];
+      return m_aligned_data[idx];
    }
 
    INLINE T& operator[](const size_t idx) {
       assert(idx < m_size);
-      return m_data[idx];
+      return m_aligned_data[idx];
    }
 
    INLINE size_t size() const {
@@ -114,29 +121,30 @@ public:
    }
 
    INLINE T* begin() {
-      return &(m_data[0]);
+      return &(m_aligned_data[0]);
    }
 
    INLINE T* end() {
-      return &(m_data[m_size]);
+      return &(m_aligned_data[m_size]);
    }
 
    INLINE T const * cbegin() const {
-      return m_data;
+      return m_aligned_data;
    }
 
    INLINE T const * cend() const {
-      return &(m_data[m_size]);
+      return &(m_aligned_data[m_size]);
    }
 
 
 private:
-   T *m_data;
    const size_t m_size;
+   T *m_aligned_data;
+   T *m_data;
 };
 
-template<class T>
-class array<T, typename std::enable_if<std::is_fundamental<T>::value>::type> final {
+template<class T, size_t ALIGNMENT>
+class array<T, ALIGNMENT, typename std::enable_if<std::is_fundamental<T>::value>::type> final {
 
    //prevent heap alloc
    static void *operator new     (size_t) = delete;
@@ -148,46 +156,55 @@ public:
    INLINE array(const size_t size)
       :
       m_size(size) {
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);	//alloca will create chunk on the stack
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+
    }
 
    INLINE array(const size_t size, const T init_value)
       :
       m_size(size) {
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);	//alloca will create chunk on the stack
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
       std::fill(begin(), end(), init_value);
    }
 
    INLINE array(std::initializer_list<T> l)
       :
       m_size(l.size()) {
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
       std::copy(l.begin(), l.end(), this->begin());
    }
 
    INLINE array(const array<typename T> &other)
       :
       m_size(other.m_size) {
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
-      memcpy(m_data, other.m_data, sizeof(T) * m_size);
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+      memcpy(m_aligned_data, other.m_aligned_data, sizeof(T) * m_size);
    }
 
    INLINE array& operator=(const array<typename T> &other) {
       m_size = other.m_size;
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
-      memcpy(m_data, other.m_data, sizeof(T) * m_size);
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+      memcpy(m_aligned_data, other.m_aligned_data, sizeof(T) * m_size);
    }
 
    INLINE array& operator=(std::initializer_list<T> other) {
       m_size = other.m_size;
-      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + 1);
+      m_data = (T*)sm_stack_alloc(sizeof(T) * m_size + ALIGNMENT + 1);	//alloca will create chunk on the stack
+      m_aligned_data = (T*)(((UINT_PTR)m_data + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
       std::copy(other.begin(), other.end(), begin());
    }
 
    INLINE array(const array<typename T> &&other) {
       m_size = other.m_size;
+      m_aligned_data = other.m_aligned_data;
       m_data = other.m_data;
       other.m_size = 0;
+      other.m_aligned_data = nullptr;
       other.m_data = nullptr;
    }
 
@@ -196,21 +213,21 @@ public:
    }
 
    INLINE T* const data() const {
-      return m_data;
+      return m_aligned_data;
    }
 
    INLINE T* data() {
-      return m_data;
+      return m_aligned_data;
    }
 
    INLINE T& operator[](const size_t idx) const {
       assert(idx < m_size);
-      return m_data[idx];
+      return m_aligned_data[idx];
    }
 
    INLINE T& operator[](const size_t idx) {
       assert(idx < m_size);
-      return m_data[idx];
+      return m_aligned_data[idx];
    }
 
    INLINE size_t size() const {
@@ -218,25 +235,26 @@ public:
    }
 
    INLINE T* begin() {
-      return &(m_data[0]);
+      return &(m_aligned_data[0]);
    }
 
    INLINE T* end() {
-      return &(m_data[m_size]);
+      return &(m_aligned_data[m_size]);
    }
 
    INLINE T const * cbegin() const {
-      return m_data;
+      return m_aligned_data;
    }
 
    INLINE T const * cend() const {
-      return &(m_data[m_size]);
+      return &(m_aligned_data[m_size]);
    }
 
 
 private:
-   T *m_data;
    const size_t m_size;
+   T *m_aligned_data;
+   T *m_data;
 };
 
 }
